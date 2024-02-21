1979881241f801a3ef35c5bf6cc2630a5441c044
naveen-m0804 <mnaveenm2004@gmail.com>
2024-02-21 20:38:30 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿using System;
-    class Eligibility
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Enter marks obtained in Maths:");
+        int mathsMarks = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter marks obtained in Physics:");
+        int physicsMarks = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter marks obtained in Chemistry:");
+        int chemistryMarks = Convert.ToInt32(Console.ReadLine());
+
+        int totalMarks = mathsMarks + physicsMarks + chemistryMarks;
+
+        if (mathsMarks >= 65 && physicsMarks >= 55 && chemistryMarks >= 50 && (totalMarks >= 180 || (mathsMarks + physicsMarks) >= 140))
         {
-           Console.WriteLine("Hiiiiiii");
+            Console.WriteLine("Congratulations! You are eligible for admission to the engineering course.");
+        }
+        else
+        {
+            Console.WriteLine("You are not eligible for admission to the engineering course.");
         }
     }
-
+}
